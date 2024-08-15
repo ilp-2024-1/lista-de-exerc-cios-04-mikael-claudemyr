07747a0a0576dd0d65dedbefd5a6cafb7945a5da
ClaudemyrMedeiros <165174653+ClaudemyrMedeiros@users.noreply.github.com>
2024-08-15 10:15:18 -0300

    ajustes

--- testes_claudemyr.py
+++ testes_claudemyr.py
@@ -4,11 +4,70 @@
 # Dica: use o operador “%” para verificar se o número é par (ZERO é neutro, ZERO NÃO
 # É PAR). Exemplos:
 
+# lista=[]
+# quant=0
+# for x in range(10):
+#     lista.append(int(input('Valor {}: '.format(x+1))))
+# for xx in lista:
+#     if xx %2 ==0 and xx !=0:
+#             quant+=1               
+# print(f'Quantidade de números pares é: {quant}')
+
+# 3. leia 10 valores inteiros e armazene em uma lista. O programa deve imprimir no terminal
+# quantos valores pares foram digitados pelo usuário. Dica: use o operador “%” para
+# verificar se o número é par (ZERO é neutro, ZERO NÃO É PAR). Exemplos:
+
+# lista=[]
+# quant=0
+# for x in range(10):
+#     lista.append(int(input('Valor {}: '.format(x+1))))
+# for xx in lista:
+#     if xx %2 ==0 and xx !=0:
+#             quant+=1               
+# print(f'Quantidade de números pares é: {quant}')
+
+# 4. Escreva um programa que receba como entrada 3 parâmetros: um valor inteiro corres-
+# pondente à quantidade de elementos de dois arrays unidimensionais; duas sequências
+# de valores do tamanho do primeiro parâmetro, os quais deverão ser armazenados em
+# duas listas distintas. Em seguida, o programa deverá criar uma lista resultante formada
+# pela intercalação dos valores de cada uma das sequências de entrada. Como saída o
+# programa deverá imprimir as duas listas iniciais e a lista resultante. Exemplo:
+
+
+# quantidade_elementos=3
+# lista1=[]
+# lista2=[]
+# lista3=[]
+
+# for x in range(quantidade_elementos):
+#     lista1.append(input('{}º da lista 1: '.format(x+1))) 
+
+# for x in range(quantidade_elementos):
+#     lista2.append(input('{}º da lista 2: '.format(x+1))) 
+
+# for i in range(len(lista1)):
+#     lista3.append(lista1[i])
+#     lista3.append(lista2[i])
+
+# print(f'Ver resultado 1 : lista 1: {lista1} lista2: {lista2} lista 3(intercalada): {lista3}')
+
+# 5. Escreva um programa que receba como entrada uma sequência de valores inteiros.
+# Para tanto, o programa deverá inicialmente solicitar ao usuário quantos valores serão
+# fornecidos para análise e só depois solicitar os valores a serem analisados. A análise
+# consistirá em identificar e apresentar a partir da sequência de valores fornecidos, o
+# menor valor, o maior valor e a média aritmética dos valores. Exemplos:
+
 lista=[]
-quant=0
-for x in range(10):
-    lista.append(int(input('Valor {}: '.format(x+1))))
-for xx in lista:
-    if xx %2 ==0 and xx !=0:
-            quant+=1               
-print(f'Quantidade de números pares é: {quant}')
+lista2=[]
+listaOrdenada=[]
+qnt_usuario=int(input('Digite a quantidade de valores: '))
+
+for x in range(qnt_usuario):
+    lista.append(int(input('{}º valor da lista: '.format(x+1))))
+
+for y in lista:
+    while lista:
+        if y < lista[y]:
+            lista2.append(y)
+    break
+print(lista2)
