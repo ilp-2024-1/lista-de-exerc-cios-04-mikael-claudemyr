68581b0c0594646de3f39b548e788dcdef23915d
ClaudemyrMedeiros <165174653+ClaudemyrMedeiros@users.noreply.github.com>
2024-08-19 08:14:44 -0300

    Ajustes questão 4 e 5

--- testes_claudemyr.py
+++ testes_claudemyr.py
@@ -51,24 +51,55 @@
 
 # print(f'Ver resultado 1 : lista 1: {lista1} lista2: {lista2} lista 3(intercalada): {lista3}')
 
-# 5. Escreva um programa que receba como entrada uma sequência de valores inteiros.
+# 5  Escreva um programa que receba como entrada uma sequência de valores inteiros.
 # Para tanto, o programa deverá inicialmente solicitar ao usuário quantos valores serão
 # fornecidos para análise e só depois solicitar os valores a serem analisados. A análise
 # consistirá em identificar e apresentar a partir da sequência de valores fornecidos, o
 # menor valor, o maior valor e a média aritmética dos valores. Exemplos:
 
 
-# 8. Escreva um programa que receba como entrada uma sequência de valores inteiros.
-# Para tanto, o programa deverá inicialmente solicitar ao usuário quantos valores serão
-# fornecidos para análise e só depois solicitar os valores a serem analisados. A análise
-# consistirá em identificar e apresentar a partir da sequência de valores fornecidos, o
-# menor valor, o maior valor e a média aritmética dos valores. Exemplos:
+# listaOrdenada=[]
+# listaSemOrdem=[]
 
-lista = []
-menor=0
-nrValores = int(input("Você deseja analisar quantos valores? "))
+# n=int(input('Forneça a quantidade de valores inteiros: '))
+# total=2
+# # aqui fizemos a ordenação para saber qual é o menor e maior número
+# for x in range(n):
+#     n = int(input('Digite o {}º valor: '.format(x+1)))
+#     listaSemOrdem.append(n)
+#     incluido = False
+#     for i in range(len(listaOrdenada)):
+#         if n<listaOrdenada[i] :
+#             listaOrdenada.insert(i, n)
+#             incluido = True
+#             break
 
-for x in range(nrValores):
-    lista.append(int(input('Informe o valor {}: '.format(x+1))))
-for xx in lista:
-        print(lista)
+#     if not incluido:
+#         listaOrdenada.append(n)
+
+# # aqui eu somo e divido pela quantidade de numeros para chegar na média
+# vr=0
+# media=0
+# for y in range(len(listaOrdenada)):
+#     vr+=listaOrdenada[y]
+# media=vr/len(listaOrdenada)
+
+# print(f' O Menor número é: {listaOrdenada[0]} , O maior é: {listaOrdenada[-1]} a média é : {media: ,.2f}')
+
+
+# 8. Escreva um programa que receba como entrada uma string constituída por uma se-
+# quência de números inteiros separados por espaço. O programa deverá transformar
+
+# essa string em uma lista de números inteiros e apresentar o resultado da soma dos
+# valores das posições ímpares dessa lista. Exemplos:
+
+# strSequencia= '25 44 65 22 88 35 98 10 55 23 633 5 87 55'
+# Nlista= []
+# total=0
+
+# Nlista=strSequencia.split()
+
+# for x in range(len(Nlista)):
+#     if x %2 != 0:
+#         total+=int(Nlista[x])
+# print(total)
